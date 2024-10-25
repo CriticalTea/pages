1adace25b7caf1d0855d4c48179337fc68e56910
Martin Salzer <mail@martin-salzer.de>
2024-10-25 21:11:20 +0200

    sdfao

---
+++ mauerstrassenwetten/stocks.js
@@ -0,0 +1,199 @@
+      var stocks = [
+		  {
+		  country: 'South Africa',
+		  countryDE: 'Südafrika',
+		  isin: 'ZAE000320321',
+		  title: 'Premier Group Limited',
+		  description: 'DD: Wie ich einmal fast eine Bäckerei in Südafrika gekauft hätte...',
+		  link: 'https://www.reddit.com/r/mauerstrassenwetten/comments/1cohxcy/wie_ich_einmal_fast_eine_b%C3%A4ckerei_in_s%C3%BCdafrika/'
+	  },
+          {
+		  country: 'China',
+		  countryDE: 'China',
+		  isin: 'US8552441094',
+		  title: 'Luckin Coffee',
+		  description: 'Chinesisches Starbucks auf Steroide',
+		  link: 'https://www.reddit.com/r/mauerstrassenwetten/comments/1b9nv70/gl%C3%BCcklicher_kaffee_sternenb%C3%B6cke_bei_wunsch/'
+	  },
+          {
+		  country: 'Kazakhstan',
+		  countryDE: 'Kasachstan',
+		  isin: 'US48581R2058',
+		  title: 'Kaspi Bank',
+		  description: 'Kasachstanbande stronk!',
+		  link: ''
+	  },
+          {
+		  country: 'Ukraine',
+		  countryDE: 'Ukraine',
+		  isin: 'GB00B1XH2C03',
+		  title: 'Ferrexpo PLC',
+		  description: 'DD: Ukrainische Freiheitsaktie 🇺',
+		  link: 'https://www.reddit.com/r/mauerstrassenwetten/comments/1bhnh63/ukrainische_freiheitsaktie/'
+	  },
+          {
+		  country: 'Sweden',
+		  countryDE: 'Schweden',
+		  isin: 'SE0000936478',
+		  title: 'Intrum',
+		  description: 'Inkasso-Bumsbude aus MSW-Podcast',
+		  link: '',
+	  },
+	  /*
+	  /*
+          {
+		  country: 'Israel',
+		  countryDE: 'Israel',
+		  isin: 'IL0011766354',
+		  title: 'Bull Trading and Investment Ltd',
+		  description: 'Qualitätsbrecher',
+		  link: 'https://www.reddit.com/r/mauerstrassenwetten/comments/1cemok6/dd_israelischer_qualit%C3%A4tsbrecher/'
+	  },
+          {
+		  country: 'US',
+		  countryDE: 'USA',
+		  isin: 'US9892071054',
+		  title: 'Zebra Technologies',
+		  description: 'Beliebtes Sarafi-Gelände',
+		  link: 'https://www.reddit.com/r/mauerstrassenwetten/comments/1ca8i4j/jagd_auf_rotes_zebra_teil_4/'
+	  },
+	  */
+          {
+		  country: 'Germany',
+		  countryDE: 'Deutschland',
+		  isin: 'US09075V1026',
+		  title: 'BioNTech',
+		  description: 'Biotechbumsbude mit Impfhintergrund',
+		  link: ''
+	  },
+          {
+		  country: 'Indonesia',
+		  countryDE: 'Indonesien',
+		  isin: 'ID1000116700',
+		  title: 'Indofood CBP Sukses',
+		  description: 'Instant-Nudeln und Co (aus MSW-Podcast? Folge?)',
+		  link: ''
+	  },
+	  {
+		  country: 'Argentina',
+		  countryDE: 'Agentinien',
+		  isin: 'US2264061068',
+		  title: 'Cresud',
+		  description: 'Landwirtschaft',
+		  link: 'https://www.cresud.com.ar/index.php?lng=en'
+	  },
+	  {
+		  country: 'Brazil',
+		  countryDE: 'Brasilien',
+		  isin: 'KYG5391L1023',
+		  title: 'Lavoro Limited',
+		  description: 'Landwirtschaft',
+		  link: 'https://ir.lavoroagro.com/'
+	  },
+	  {
+		  country: 'Hawaii',
+		  countryDE: 'Hawaii',
+		  isin: 'US5773451019',
+		  title: 'Maui Land & Pineapple Company',
+		  description: 'Landwirtschaft',
+		  link: ''
+	  },
+	  {
+		  country: 'Australia',
+		  countryDE: 'Australien',
+		  isin: 'AU000000ELD6',
+		  title: 'Elders Limited',
+		  description: 'Landwirtschaft',
+		  link: ''
+	  },
+	  {
+		country: 'Estonia',
+		countryDE: 'Estland',
+		isin: 'EE3100034653',
+		title: 'Arco Vara AS',
+		description: 'Immobilienbude mit Ex-Rennfahrer als CEO',
+		link: 'https://arcovara.com/en/'
+	  },
+      {
+		country:'Cayman Islands',
+		countryDE:'Cayman Islands',
+		 isin: 'KYG237731073',
+		 title: 'Consolidated Water',
+		 description: 'Karibische Meerwasserentsalzung macht brrr',
+		 link: 'https://www.reddit.com/r/mauerstrassenwetten/comments/1bihbrw/karibische_meerwasserentsalzung_macht_brrr/'
+	  },
+	  {
+		country: 'Japan',
+		countryDE: 'Japan',
+		isin: 'JP3756600007',
+		title: 'Nintendo',
+		description: 'Ein Stück Kindheit',
+		link: ''
+	  },
+	  {
+		country: 'US',
+		countryDE: 'USA',
+		isin: 'US75734B1008',
+		title: 'Reddit',
+		description: 'Hassliebe',
+		link: ''
+	  },
+	  {
+		country: 'United Kingdom',
+		countryDE: 'Großbritannien',
+		isin: 'GB00BYVX2X20',
+		title: 'Team17',
+		description: 'Worms Spiele und Co',
+		link: ''
+	  },
+	  {
+		country: 'Thailand',
+		countryDE: 'Thailand',
+		isin: 'TH0902010014',
+		title: 'Thai Beverage',
+		description: 'Wenn dein Business so geil ist, dass Mönche gegen deinenn Börsengang demonstrieren, dann machst du was richtig',
+		link: ''
+	  },
+	  {
+		country: 'France',
+		countryDE: 'Frankreich',
+		isin: 'FR0000054470',
+		title: 'Ubisoft Entertainment',
+		description: 'Anno 1800',
+		link: ''
+	  },
+	  {
+		country: 'Finland',
+		countryDE: 'Finnland',
+		isin: 'FI0009006308',
+		title: 'HKFoods Oyj',
+		description: 'Fleischerei-Produkte Nordeuropa für Retail',
+		link: ''
+	  },
+	  {
+		country: 'Norway',
+		countryDE: 'Norwegen',
+		isin: 'NO0011082075',
+		title: 'Höegh Autoliners',
+		description: 'r/Finanzen-Tipp, RoRo-Rederei, Transportiert vor allem Autos',
+		link: ''
+	  },
+	  {
+		country: 'Colombia',
+		countryDE: 'Kolumbien',
+		isin: 'US2791581091',
+		title: 'Ecopetrol',
+		description: 'Ölbumsbude, ehemals Empresa Colombiana de Petróleos S.A.',
+		link: 'https://www.reddit.com/r/mauerstrassenwetten/comments/1e4ctwz/comment/ldfoiku/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button'
+	  },
+	  /*
+	  {
+		country: '',
+		countryDE: '',
+		isin: '',
+		title: '',
+		description: '',
+		link: ''
+	  },
+	  */
+      ];
